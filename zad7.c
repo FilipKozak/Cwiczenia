cd4b96475a97681fd1f6993be3cc290579085494
FilipKozak <57020177+FilipKozak@users.noreply.github.com>
2019-10-28 17:36:34 +0100

    Update zad7.c

--- zad7.c
+++ zad7.c
@@ -5,7 +5,7 @@ int main(void)
 {
 		int odpowiedz, wynik; // podstawiamy liczby pod odpowiedz oraz wynik
 		odpowiedz = 100;
-		wynik = odpowiedz -10;
+		wynik = odpowiedz -10; //zmienna wynik jest równa zmiennej odpowiedz pomniejszonej o 10
 		printf ("Wynik: %i\n" , wynik - 5); // Od otrzymanego wyniku odejmujemy jeszcze 5 i wyświetla prawidłowy wynik
 		exit(EXIT_SUCCESS); // Informuje system, iż aplikacja została zakończona sukcesem
 }
