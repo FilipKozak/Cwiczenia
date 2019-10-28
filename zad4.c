b1951bef47a500b3a9bb5c0cfdc67bbcbbace48a
FilipKozak <57020177+FilipKozak@users.noreply.github.com>
2019-10-28 17:33:09 +0100

    Update zad4.c

--- zad4.c
+++ zad4.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
-	printf ("W języku C wielkość liter ma zanczenie.\n");
+	printf ("W języku C wielkość liter ma znaczenie.\n");
 	printf ("Wykonanie programu zaczyna się od funkcji main.\n");
 	return 0;
 }
