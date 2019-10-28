8fbe3fa7eb85ba0ce5357f4fa14b40f3887599b1
FilipKozak <57020177+FilipKozak@users.noreply.github.com>
2019-10-28 17:34:08 +0100

    Update zad5.c

--- zad5.c
+++ zad5.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main(void)
 {
-			//Obliczanie wyniku
+			//Obliczanie różnicy dwóch liczb
 	 int sum = 87 - 15;
-	 	 	//Wyświetlanie wyniku
+	 	 	//Wyświetlanie wyniku odejmowania
 	printf ("Róznica 87-15 wynosi: %i\n", sum);
 	return 0;
 }
